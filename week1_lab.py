a5316c9ae2ac919e365efd1ed916f8c335011e6d
APrescott24 <aaronprescott2012@hotmail.co.uk>
2025-10-07 15:20:52 +0100

    Add files via upload

---
+++ week1_lab.py
@@ -0,0 +1,19 @@
+from ast import Add
+
+
+student_name = "Alex"
+
+student_age = 20
+
+current_year = 2025
+
+print("Student Name:", student_name)
+print("Student Age:", student_age)
+
+YOB = current_year - student_age
+
+print("Year of Birth:", YOB)
+
+git Add week1_lab.py
+git commit -m "Feat: Completed week 1 lab exercises"
+git push
